69a81b655f700b94bbb796431cc4196539dfd43a
Kirill Volkov <kirillvolkov@team.amocrm.com>
2024-12-03 18:18:25 +0300

    delete unused files

--- internal/repository/contacts/model/contacts.go
+++
@@ -1,8 +0,0 @@
-package entities
-
-type Contacts struct {
-	ID        int64  `json:"id"`
-	Name      string `json:"name"`
-	AccountID int    `json:"account_id"`
-	Email     string `json:"email"`
-}
