7bc8021e7a0015e537cc6712359bb632aa14e4c7
Kirill Volkov <kirillvolkov@team.amocrm.com>
2024-12-09 18:28:25 +0300

    delete comments

--- internal/service/account/methods.go
+++ internal/service/account/methods.go
@@ -12,8 +12,6 @@ import (
 
 const authURL = "https://volkovkirill.amocrm.ru/oauth2/access_token"
 
-// const contactsURL = "https://volkovkirill.amocrm.ru/api/v4/contacts"
-
 func (s *Service) GetAccountByID(accountID int64) (entities.Account, error) {
 	account, err := s.accountRepository.GetAccountByID(accountID)
 	if err != nil {
@@ -22,125 +20,6 @@ func (s *Service) GetAccountByID(accountID int64) (entities.Account, error) {
 
 	return account, nil
 }
-
-// func (s *Service) GetContactsByAccountID(accountID int64) ([]entities.Contacts, error) {
-// 	account, err := s.accountRepository.GetAccountByID(accountID)
-// 	if err != nil {
-// 		log.Printf("Error get account by ID %d: %v", accountID, err)
-// 		return nil, err
-// 	}
-
-// 	req, err := http.NewRequest("GET", contactsURL, nil)
-// 	if err != nil {
-// 		return nil, err
-// 	}
-
-// 	req.Header.Add("Authorization", "Bearer "+account.AccessToken)
-
-// 	resp, err := http.DefaultClient.Do(req)
-// 	if err != nil {
-// 		return nil, errors.New(entities.ErrNotFoundAcc)
-// 	}
-// 	defer resp.Body.Close()
-
-// 	body, err := io.ReadAll(resp.Body)
-// 	if err != nil {
-// 		return nil, errors.New(entities.ErrReadBody)
-// 	}
-
-// 	if resp.StatusCode != http.StatusOK {
-// 		return nil, errors.New(entities.ErrGetContacts)
-// 	}
-
-// 	var apiResponse entities.ContactAPIResponse
-// 	if err := json.Unmarshal(body, &apiResponse); err != nil {
-// 		return nil, errors.New(entities.ErrParse)
-// 	}
-
-// 	var contacts []entities.Contacts
-// 	for _, contact := range apiResponse.Embedded.Contacts {
-// 		var email string
-// 		for _, field := range contact.CustomFieldsValues {
-// 			if field.FieldName == "Email" && len(field.Values) > 0 {
-// 				email = field.Values[0].Value
-// 				break
-// 			}
-// 		}
-
-// 		contacts = append(contacts, entities.Contacts{
-// 			Name:  contact.Name,
-// 			Email: email,
-// 		})
-// 	}
-
-// 	return contacts, nil
-// }
-
-// func (s *Service) GetAllContacts() ([]entities.Contacts, error) {
-
-// 	accounts, err := s.accountRepository.GetAllAccounts()
-// 	if err != nil {
-// 		return nil, errors.New(entities.ErrNotFoundAcc)
-// 	}
-
-// 	var allContacts []entities.Contacts
-
-// 	for _, account := range accounts {
-// 		req, err := http.NewRequest("GET", contactsURL, nil)
-// 		if err != nil {
-// 			return nil, err
-// 		}
-
-// 		req.Header.Add("Authorization", "Bearer "+account.AccessToken)
-
-// 		resp, err := http.DefaultClient.Do(req)
-// 		if err != nil {
-// 			return nil, errors.New(entities.ErrNotFoundContacts)
-// 		}
-
-// 		defer resp.Body.Close()
-
-// 		log.Println("Status Code:", resp.StatusCode)
-// 		log.Println("Using AccessToken:", account.AccessToken)
-
-// 		if resp.StatusCode != http.StatusOK {
-// 			body, err := io.ReadAll(resp.Body)
-// 			if err != nil {
-// 				return nil, errors.New(entities.ErrReadBody)
-// 			}
-// 			log.Printf("API Response: %s\n", string(body))
-// 			return nil, errors.New(entities.ErrGetContacts)
-// 		}
-
-// 		body, err := io.ReadAll(resp.Body)
-// 		if err != nil {
-// 			return nil, errors.New(entities.ErrReadBody)
-// 		}
-// 		log.Printf("API Response Body: %s\n", string(body))
-
-// 		var apiResponse entities.ContactAPIResponse
-// 		if err := json.Unmarshal(body, &apiResponse); err != nil {
-// 			return nil, errors.New(entities.ErrParse)
-// 		}
-// 		for _, contact := range apiResponse.Embedded.Contacts {
-
-// 			var email string
-// 			for _, field := range contact.CustomFieldsValues {
-// 				if field.FieldName == "Email" && len(field.Values) > 0 {
-// 					email = field.Values[0].Value
-// 					break
-// 				}
-// 			}
-
-// 			allContacts = append(allContacts, entities.Contacts{
-// 				Name:  contact.Name,
-// 				Email: email,
-// 			})
-// 		}
-
-// 	}
-// 	return allContacts, nil
-// }
 
 func (s *Service) Authorization(request entities.AuthRequest) (entities.Account, error) {
 	body, err := json.Marshal(request)
